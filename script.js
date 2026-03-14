676fdb525b77bb5f9735435e3b1fb2cf5eb32bb8
Gabriel Mendes <gabrelmendes7@gmail.com>
2026-03-14 14:23:06 -0300

    Adiocionando a logica de operacoes matematicas no arq .js

---
+++ script.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const display = document.querySelector("label");
+    const botoes = document.querySelectorAll("button");
+
+    let expressao = "";
+
+    botoes.forEach(botao => {
+        botao.addEventListener("click", function () {
+
+            let valor = this.innerText;
+
+            if (valor === "=") {
+                try {
+                    expressao = expressao.replace("X", "*");
+                    display.innerText = eval(expressao);
+                    expressao = display.innerText;
+                } catch {
+                    display.innerText = "Erro";
+                    expressao = "";
+                }
+            }
+
+            else if (valor === "LT") {
+                expressao = "";
+                display.innerText = "0";
+            }
+
+            else if (valor === "L") {
+                expressao = expressao.slice(0, -1);
+                display.innerText = expressao || "0";
+            }
+
+            else {
+                expressao += valor;
+                display.innerText = expressao;
+            }
+
+        });
+    });
+
+});
